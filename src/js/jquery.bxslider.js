15fe976979e059c74442b87c056f90e12965f3c0
Lyle Mantooth <lyle.mantooth@primasupply.com>
2019-10-25 11:05:32 -0400

    Apply touch-scroll fix.

--- src/js/jquery.bxslider.js
+++ src/js/jquery.bxslider.js
@@ -368,7 +368,7 @@
           // add the current slide to the children
           children = slider.children.eq(currentIndex);
           // cycle through the remaining "showing" slides
-          for (i = 1; i <= slider.settings.maxSlides - 1; i++) {
+          for (var i = 1; i <= slider.settings.maxSlides - 1; i++) {
             // if looped back to the start
             if (currentIndex + i >= slider.children.length) {
               children = children.add(slider.children.eq(i - 1));
@@ -1106,7 +1106,6 @@
       if (e.type !== 'touchstart' && e.button !== 0) {
         return;
       }
-      e.preventDefault();
       //disable slider controls while user is interacting with slides to avoid slider freeze that happens on touch devices when a slide swipe happens immediately after interacting with slider controls
       slider.controls.el.addClass('disabled');
 
@@ -1185,15 +1184,17 @@
       // this is swipe
       slider.hasMove = true;
 
-      // x axis swipe
-      if ((xMovement * 3) > yMovement && slider.settings.preventDefaultSwipeX) {
-        e.preventDefault();
-      // y axis swipe
-      } else if ((yMovement * 3) > xMovement && slider.settings.preventDefaultSwipeY) {
-        e.preventDefault();
-      }
-      if (e.type !== 'touchmove') {
-        e.preventDefault();
+      if (e.hasOwnProperty('cancelable') && e.cancelable) {
+        // x axis swipe
+        if ((xMovement * 3) > yMovement && slider.settings.preventDefaultSwipeX) {
+          e.preventDefault();
+        // y axis swipe
+        } else if ((yMovement * 3) > xMovement && slider.settings.preventDefaultSwipeY) {
+          e.preventDefault();
+        }
+        if (e.type !== 'touchmove') {
+          e.preventDefault();
+        }
       }
 
       if (slider.settings.mode !== 'fade' && slider.settings.oneToOneTouch) {
